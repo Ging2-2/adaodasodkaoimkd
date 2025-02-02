68936c37bb0a452e675b1ad3caeec3051fb88248
Ging2-2 <syn4mylife@gmail.com>
2025-02-02 19:53:32 -0400

    Pandora

--- Rivals.lua
+++ Rivals.lua
@@ -7,21 +7,21 @@ if not getgenv().PandoraExecuted then
     getgenv().PandoraExecuted = true
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Aimlock.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/ESP.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Speed.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Noclip.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Teleport.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/TpFollow.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Flight.lua", true))()
-    task.wait(0.1)
+    task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/HitboxExpander.lua", true))()
-    task.wait(0.2)
+    task.wait(1)
 
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = " 🔥 Pandora 🔥", 
